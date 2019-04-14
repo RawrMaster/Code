35ac44a2b6947de95914c144b6167d32432e1267
RawrMaster <44449114+RawrMaster@users.noreply.github.com>
2019-04-14 21:57:00 +0300

    Update ahocor.py

--- string-similarity-thd_modified/core/methods/ahocor.py
+++ string-similarity-thd_modified/core/methods/ahocor.py
@@ -24,8 +24,6 @@ class AhoCorasik(Method):
             res = A.match(key, 'not exists')
             summary += int(res) # собираем все баллы
 
-     #   print(">>>> longer: ", longer, " shorter : ", shorter, " result: ", summary)
-
         score = summary / len(arr_shorter) # находим среднее
      #   print(">>>>score: ", score)
         return score
@@ -42,31 +40,3 @@ class AhoCorasik(Method):
     def _build_tokens(word: str) -> set:
         return set(word)
 
-
-# class Jaccard(Method):
-#     def __init__(self):
-#         self.first_tokens = None
-#         self.second_tokens = None
-#
-#     def score(self, w1: str, w2: str) -> float:
-#         self.first_tokens = self._build_tokens(word=w1)
-#         self.second_tokens = self._build_tokens(word=w2)
-#         print ("self.first_tokens:" , self.first_tokens)
-#         print ("self.second_tokens:" , self.second_tokens)
-#
-#         intersection_cardinality = float(len(self.intersection))
-#         union_cardinality = float(len(self.union))
-#
-#         return intersection_cardinality / union_cardinality
-#
-#     @property
-#     def intersection(self) -> set:
-#         return set.intersection(self.first_tokens, self.second_tokens)
-#
-#     @property
-#     def union(self) -> set:
-#         return set.union(self.first_tokens, self.second_tokens)
-#
-#     @staticmethod
-#     def _build_tokens(word: str) -> set:
-#         return set(word)
